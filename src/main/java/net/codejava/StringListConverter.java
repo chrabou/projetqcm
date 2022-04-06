e8f927d4149f5192221fc69352a8d821f6e747cf
MENGUELTI Hacène <hacenemenguelti@gmail.com>
2022-04-06 04:59:07 +0200

    MAJ pré-rapport (non complet)
    J'ajoute ce commit avec le "strict-minimum" sur les pages essentiels pour qu'on ai les screens pour le rendu du rapport.
    Cependant il manque certains ajouts qui ne sont pour le moment pas dans cette version, dans un premier temps a cause de certain soucis d'affichage sur le page et dans un second temps d'un soucis de "responsivité" des pages webs.

---
+++ src/main/java/net/codejava/StringListConverter.java
@@ -0,0 +1,36 @@
+package net.codejava;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+import static java.util.Collections.*;
+
+@Converter
+public class StringListConverter implements AttributeConverter<List<String>, String> {
+    private static final String SPLIT_CHAR = ";";
+    
+    @Override
+    public String convertToDatabaseColumn(List<String> stringList) {
+    	if(stringList == null) {
+    		return "";
+    	}
+    	
+    	  for (int i = 0; i < stringList.size(); ++i) {
+              stringList.set(i, stringList.get(i).replaceAll(";", ""));
+          }
+        return stringList != null ? String.join(SPLIT_CHAR, stringList) : "";
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute(String string) {
+        return string != null ? Arrays.asList(string.split(SPLIT_CHAR)) : emptyList();
+    }
+    
+    public static List<String> convertToEntityAttribute2(String string) {
+        return string != null ? Arrays.asList(string.split(SPLIT_CHAR)) : emptyList();
+    }
+}
---
+++ src/main/java/net/codejava/WebSecurityConfig.java
@@ -0,0 +1,71 @@
+package net.codejava;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomUserDetailsService();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/users").authenticated()
+			.antMatchers("/internaute").authenticated()
+			.antMatchers("/directeur").authenticated()
+			.antMatchers("/responsable").authenticated()
+			.antMatchers("/employe").authenticated()
+			.antMatchers("/creation_qcm").authenticated()
+			.antMatchers("/process_creation").authenticated()
+			.antMatchers("/process_modification").authenticated()
+			.antMatchers("/faireQcm").authenticated()
+			.antMatchers("/detailQcm").authenticated()
+			.antMatchers("/detailStatQcm").authenticated()
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+				.usernameParameter("email")
+				.successHandler(loginSuccessHandler)
+				.permitAll()
+			.and()
+			.logout().logoutSuccessUrl("/").permitAll();
+	}
+	@Autowired private LoginSuccessHandler loginSuccessHandler;
+	
+}
